4d931a54b53c625f929ff23afe39bd74e0a5d6e0
anfegagra <andres.granados@ceiba.com.co>
2020-02-10 20:25:53 -0500

    se crea archivo xml para hacer configuración de beans basado en XML

--- src/main/java/com/chucknorris/app/ChucknorrisApplication.java
+++ src/main/java/com/chucknorris/app/ChucknorrisApplication.java
@@ -2,8 +2,10 @@ package com.chucknorris.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-norris-config.xml")
 public class ChucknorrisApplication {
 
 	public static void main(String[] args) {
